b717f5f39b086c7ee1b027250e5a1e12bba31b01
Ramanuj Das <ramanujdas@Ramanujs-MacBook-Pro.local>
2023-07-27 14:25:02 +0530

    api isuues fixed

--- src/main/java/com/bookstoreapp/controller/ApiController.java
+++ src/main/java/com/bookstoreapp/controller/ApiController.java
@@ -2,6 +2,7 @@ package com.bookstoreapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ApiController {
@@ -13,6 +14,7 @@ public class ApiController {
 
 
     @GetMapping("/")
+    @ResponseBody
     public String showWelcomeMessage() {
         return "Welcome to Bookstore App";
     }
